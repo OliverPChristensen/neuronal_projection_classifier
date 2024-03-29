364da9872cc9d79eb9117df56971803ed19006da
wqc597 <wqc597@esrumhead01fl.unicph.domain>
2024-03-29 15:39:35 +0100

    Added commments and documentation to segmentation and spatial seurat pipeline

--- scripts/generate_spatial_objects.R
+++ scripts/generate_spatial_objects.R
@@ -6,42 +6,84 @@ library(qs)
 
 
 load_transcripts <- function(transcripts_path){
+    # Input: Path to transcript file
+    #
+    # Loades the transcript file, removes false positive (FP) transripts and creates a sf object from the trasncript dataframe
+    #
+    # Output: transcripts as a sf object with POINT geometries
+
+    # Read transcripts file and remove false positive transcripts
     transcripts <- read.table(transcripts_path, header = F, sep = "\t") %>% filter(!str_detect(V4, "FP "))
+
+    # Creates sf object from transcript data frame with POINT geometries
     transcript_points <- transcripts %>% st_as_sf(.,coords= c("V1","V2"))
     
     return(transcript_points)
 }
 
-array_to_sf_rois <- function(array_path){
-    # Load the NumPy module
+array_to_sf_rois <- function(rois_path){
+    # Input: Path to rois file saved in numpys npz format
+    #
+    # Loads rois as numpy arrays and converts them to sfc objects containing rois as POLYGON geometries
+    #
+    # Output: sfc object containing rois as POLYGON geometries
+
+    # Import numpy module
     np <- import("numpy")
 
-    # Load the .npz file
-    arrays <- np$load(array_path)
+    # Load rois as arrays
+    arrays <- np$load(rois_path)
 
+    # Convert rois to a list of POLYGON objects
     rois <- list()
     for (roi in arrays$files){
         roi_coords <- arrays$get(roi)
 
-        #Closing the roi coords
+        # Closing the roi coords
         roi_coords_closed <- rbind(roi_coords,roi_coords[1,])
         rois[[roi]] <- st_polygon(list(roi_coords_closed))
     }
 
+    # Convert the list of polygons to sfc object and return the object
     return(rois %>% st_sfc())
 }
 
 create_count_matrix <- function(transcripts, rois){
+    # Input: Transcripts as sf object with POINT geometries, ROIs as sfc object with POLYGON geometries
+    #
+    # Intersects the transcripts and ROIs objects which return a sparse list (sgbp) of individuel transcript-cell pairs.
+    # This list of pairs converted to a sparse matrix with counts of each transcript-cell pair
+    #
+    # Output: Sparse count matrix with cells as columns and genes as rows
+
+    # Intersect transcripts and ROIs objects
     sparse_intersects <- st_intersects(transcripts,rois,sparse = T)
+
+    # Name the transcripts of the intersects list
     names(sparse_intersects) <- transcripts$V4
+
+    # Convert the intersect list to vectors. This process only returns non-empty elements in the intersect list (that is, transcripts that intersects a cell)
+    # Then the transcript-cell pairs are counted using table
     sparse_count_matrix <- as(table(names(unlist(sparse_intersects)),unlist(sparse_intersects)),"sparseMatrix")
     
+    # Give cells a cell ID
     colnames(sparse_count_matrix) <- paste0("Cell",colnames(sparse_count_matrix))
+
     return(sparse_count_matrix)
 }
 
 create_seurat_segmentations <- function(rois,sparse_count_matrix){
+    # Input: ROIs as sfc object with POLYGON geometries, sparse count matrix with transcripts as rows and cells as columns
+    #
+    # Converts the sfc ROIs object to a data frame with column names that fits expected format by Seurat.
+    # Filters cells that do not intersect any transcripts
+    #
+    # Output: Data frame with rois in Seurat format
+
+    # Retrieve coordinates from sfc object, create cell IDs and filter cells that did not intersect any transcripts
     rois_df <- rois %>% st_coordinates() %>% as.data.frame() %>% mutate(L2 = paste0("Cell",L2)) %>% filter(L2 %in% colnames(sparse_count_matrix))
+
+    # Extract information and change column names according to the Seurat format
     seurat_segmentations <- rois_df[,c("L2","X","Y")]
     colnames(seurat_segmentations) <- c("cell","x","y")
 
@@ -49,26 +91,58 @@ create_seurat_segmentations <- function(rois,sparse_count_matrix){
 }
 
 create_seurat_centroids <- function(rois,sparse_count_matrix){
+    # Input: ROIs as sfc object with POLYGON geometries, sparse count matrix with transcripts as rows and cells as columns
+    #
+    # Extracts the centroids from the cell ROIs as data frame with format that fits expected by Seurat.
+    #
+    # Output: Data frame with centroids of cells in Seurat format
+
+    # Extract centroids from ROIs
     centroids_df <- rois %>% st_centroid() %>% st_coordinates()
+
+    # Add cell IDs and filter cells that did not intersect any transcripts
     seurat_centroids <- cbind(1:nrow(centroids_df),centroids_df) %>% as.data.frame() %>% mutate(V1 = paste0("Cell",V1)) %>% filter(V1 %in% colnames(sparse_count_matrix))
+
+    # Change column names according to the Seurat format
     colnames(seurat_centroids) <- c("cell","x","y")
 
     return(seurat_centroids)
 }
 
 create_seurat_molecules <- function(transcripts){
+    # Input: Transcripts as sf object with POINT geometries
+    #
+    # Converts sf transcript object to a data frame with format that fits expected by Seurat. 
+    #
+    # Output: Data frame with transcripts in Seurat format
+
+    # Extract coordinates of transcripts from sf object and add trasncript ID
     seurat_molecules <- transcripts %>% st_coordinates() %>% as.data.frame() %>% cbind(.,transcript_points$V4)
+
+    # Change column names according to the Seurat format
     colnames(seurat_molecules) <- c("x","y","gene")
 
     return(seurat_molecules)
 }
 
 create_seurat_object <- function(sparse_count_matrix, seurat_segmentations, seurat_centroids, seurat_molecules, section){
+    # Input: Sparse count matrix with transcripts as rows and cells as columns, data frame with cell segmetation in Seurat format, 
+    #        data frame with cell centroids in Seurat format, data frame with transcripts (molecules) in Seurat format, section index
+    #
+    # Creates spatial Seurat object using the sparse count matrix and various data frames containing segmentation, centroids and transcripts in Seurat format
+    #
+    # Output: Spatial Seurat object
+
+    # Create Seurat object from the sparse count matrix
+    spatial_obj <- SeuratObject::CreateSeuratObject(counts = sparse_count_matrix, assay = 'raw')
+
+    # Create coords list containing Seurat centroids object and Seurat segmentation object form the Seurat centroids data frame and Seurat segmentation data frame
     coords_list <- list(
        "centroids" = SeuratObject::CreateCentroids(seurat_centroids),
        "segmentation" = SeuratObject::CreateSegmentation(seurat_segmentations)
     )
 
+    # Create Seurat FOV from the coords list and the Seurat transcripts data frame
     coords <- SeuratObject::CreateFOV(
        coords = coords_list,
        type = c("segmentation", "centroids"),
@@ -76,8 +150,7 @@ create_seurat_object <- function(sparse_count_matrix, seurat_segmentations, seur
        assay = 'raw'
     )
 
-    spatial_obj <- SeuratObject::CreateSeuratObject(counts = sparse_count_matrix, assay = 'raw')
-
+    # Add FOV to the Seurat object effectively making it a spatial Seurat object
     spatial_obj[[section]] <- coords
 
     return(spatial_obj)
@@ -87,10 +160,11 @@ create_seurat_object <- function(sparse_count_matrix, seurat_segmentations, seur
 
 
 
-
+# Define section indices and run index
 sections <- c("A1","B1","C1","D1","A2","B2","C2","D2")
 run <- "spatial1"
 
+# Load list of transcripts files and ROIs files
 transcripts_files <- list.files(path = paste0("./raw_data/",run,"/")) %>% grep("results", ., value = TRUE)
 rois_files <- list.files(path = paste0("./processed_data/",run,"/")) %>% grep("rois", ., value = TRUE)
 
@@ -98,33 +172,48 @@ spatial_objects = list()
 
 t <- 0
 for (section in sections){
+    # Load path to transcript file and ROIs file corresponding to current section index
     transcript_path <- paste0("./raw_data/",run,"/",grep(section, transcripts_files, value = TRUE))
     roi_path <- paste0("./processed_data/",run,"/",grep(section, rois_files, value = TRUE))
 
+    # Load transcripts
     print("Loading transcripts")
     transcript_points <- load_transcripts(transcript_path)
     
+    # Load ROIs
     print("Loading ROIs")
     rois_polygons <- array_to_sf_rois(roi_path)
 
-    print("Generating count matrix")
+    # Create sparse count matrix
+    print("Creating count matrix")
     sparse_count_matrix <- create_count_matrix(transcript_points,rois_polygons)
+
+    # Create Seurat segmentation object
     seurat_segmentations <- create_seurat_segmentations(rois_polygons,sparse_count_matrix)
+
+    # Create Seurat centroid object
     seurat_centroids <- create_seurat_centroids(rois_polygons,sparse_count_matrix)
+
+    # Create Seurat molecules object
     seurat_molecules <- create_seurat_molecules(transcript_points)
 
+    # Create spatial Seurat object
     print("Creating seurat object")
     spatial_obj <- create_seurat_object(sparse_count_matrix,seurat_segmentations,seurat_centroids,seurat_molecules,section)
     
+    # Add section index and run index as meta data
     print("Adding meta data")
-    spatial_obj$run <- run
     spatial_obj$section <- section
+    spatial_obj$run <- run
 
+    # Extract area of cells and give cell ID names to the object
     areas <- rois_polygons %>% st_area()
     names(areas) <- names(rois_polygons)
     
+    # Add the meta data to the spatial seurat object according to cell IDs
     spatial_obj <- AddMetaData(spatial_obj,areas,"area")
 
+    # Add the spatial object of the current section to the spatial object list
     spatial_objects[[section]] <- spatial_obj
 
     t <- t + 1
@@ -135,7 +224,5 @@ print("Merging spatial seurat objects")
 merged_spatial_object <- merge(spatial_objects)
 
 print("Saving merged spatial seurat object")
-qsave(merged_spatial_object,"/processed_data/",run,"/",run,"_seurat_raw.qs")
-
-
-
+dir.create(paste0("./processed_data/",run,"/",run,"_seurat_raw.qs"),showWarnings = FALSE)
+qsave(merged_spatial_object,paste0("./processed_data/",run,"/",run,"_seurat_raw.qs"))
--- scripts/prepare_data_for_seurat.py
+++ scripts/prepare_data_for_seurat.py
@@ -13,30 +13,37 @@ from cellpose import io
 from cellpose import models
 
 def current_time():
+    '''
+    Allows the current time to be added to all prints to the log, when script is run through sbatch
+    Output: Current time
+    '''
     cur_time = datetime.now()
     return cur_time.strftime("%Y-%m-%d %H:%M:%S")
 
 def staining_to_rois(image):
     '''
-    Input: Folder with polyT and dapi staining
+    Input: Numpy array of staining image
 
-    Loades and combines polyT and dapi staining (average pixel value)
     Runs cellpose on the combined staining image
-    Converts masks to list of ROIs
+    Converts masks to dictionary of ROIs
 
-    Output: ROIs for the segemnetation of the stainings
+    Output: Dictionary of ROIs for the segemnetation of the stainings
     '''
 
 
+    #Define cellpose model
     model = models.Cellpose(gpu=True, model_type = 'cyto')
 
+    #Print device
     print(f"{current_time()}: Device is {model.device}")
     sys.stdout.flush()
+
+    #Generate masks for image
     masks, _, _, diams = model.eval(image, diameter=None, channels=[0,0],
                                          flow_threshold=0.4, do_3D=False)
     
+    #Convert masks from image into ROIs and put them into a dictionary
     rois = {}
-
     for label in range(1, np.max(masks) + 1):
         mask = (masks == label).astype(np.uint8)
         contour = measure.find_contours(mask, 0.5)
@@ -44,38 +51,60 @@ def staining_to_rois(image):
 
     return rois
 
-def quality_report(image,rois,image_index,section):
+def quality_report(image,rois,run,section):
+    '''
+    Input: Numpy array of staining image, dictionary of ROIs, spatial run index, section ID
 
+    Creates and saves multiple plots of the staining image with ROIs overload. 
+    The image is split into smaller images to be able to open them with sufficient resolution in VS Code
+
+    Output: NA
+    '''
+    #Define plot and add image to plot
     fig, ax = plt.subplots()
-
     _ = ax.imshow(image)
     
+    #Add ROIs to plot
     for roi in rois:
-        #print(type(roi))
         patch = patches.Polygon(rois[roi], closed=True, edgecolor='r', facecolor='none')
         _ = ax.add_patch(patch)
-        # Add the rectangle to the plot
 
+    #Define split on each dimension of the image
     image_split_num = 50
 
+    #Calculate width of each split
     xdim = int(image.shape[1]/image_split_num)
     ydim = int(image.shape[0]/image_split_num)
     
-    os.makedirs(f"./plots/segmentation_report/{image_index}/{section}",exist_ok=True)
+    #Create folder to save plots
+    os.makedirs(f"./plots/segmentation_report/{run}/{section}",exist_ok=True)
 
+    #Generate all plots as specified by the splits. Empty images are ignored
     for i in range(image_split_num):
         for j in range(image_split_num):
             
+            #Checks if cropped image is empty. If not, then save plot
             if np.max(image[ydim*j:ydim*(j + 1),xdim*i:xdim*(i + 1)]) > 0:
-                _ = ax.set_xlim(xdim*i, xdim*(i + 1))  # Adjust these values as needed
-                _ = ax.set_ylim(ydim*j, ydim*(j + 1))  # Adjust these values as needed
+                _ = ax.set_xlim(xdim*i, xdim*(i + 1))
+                _ = ax.set_ylim(ydim*j, ydim*(j + 1))
 
-                plt.savefig(f"./plots/segmentation_report/{image_index}/{section}/{section}_quality_report_{i}-{j}.png")
-
-
+                plt.savefig(f"./plots/segmentation_report/{run}/{section}/{section}_quality_report_{i}-{j}.png")
 
 
-def create_rois(folder_with_stainings,spatial_index):
+
+
+def create_rois(folder_with_stainings,run):
+    '''
+    Input: Folder with staining images, spatial run index
+
+    Loades polyT and dapy stainings in corresponding pairs and combines them using the average pixel value
+    Forwards the image to stainings_to_rois to generate segmentation
+    Saves the ROIs and create quality report of segmentation
+
+    Output: NA
+    '''
+
+    #Dictionary to convert between naming schemes for different sections
     name_con_dict = {
         "W0": "A1",
         "W1": "B1",
@@ -87,41 +116,54 @@ def create_rois(folder_with_stainings,spatial_index):
         "W7": "D2"
     }
 
+    #Load files in folder with stanings
     files = os.listdir(folder_with_stainings)
 
+    #Fetch all dapi and polyT file names
     dapi_list = np.array([element for element in files if "DAPI" in element])
     polyT_list = np.array([element for element in files if "Cy5" in element])
 
+    #Define variable for keys in name_con_dict to be used later
     name_con_keys = np.array(list(name_con_dict.keys()))
 
+    #Loop over all polyT file names
     for i, polyT_file in enumerate(polyT_list):
 
+        #Find the section index key present in the polyT file name
         name_con_ele = name_con_keys[np.core.defchararray.find(polyT_file, name_con_keys) > 0]
 
+        #Fetch the value paired to the section index key
         name_con_ele_pair = name_con_dict[name_con_ele.item()]
+
+        #Find the dapi file name that matches the section index value such that the dapi file paired to the polyT file is now found
         dapi_file = dapi_list[np.core.defchararray.find(dapi_list,name_con_ele_pair) > 0][0]
 
+        #Load paired dapi and polyT stainings
         dapi_image = io.imread(f"{folder_with_stainings}/{dapi_file}")
         polyT_image = io.imread(f"{folder_with_stainings}/{dapi_file}")
 
+        #Combine dapi and polyT stainings by using average pixel value
         image = (dapi_image + polyT_image)/2
 
-        print(f"{current_time()}: Running cellpose on image {i+1}/{len(polyT_list)}")
+        #Input the combined image to staining_to_rois, which returns cell ROIs for the image
+        print(f"{current_time()}: Running cellpose on section {name_con_ele_pair} ({i+1}/{len(polyT_list)})")
         sys.stdout.flush()
         rois = staining_to_rois(image)
 
-        print(f"{current_time()}: Saving rois for image {i+1}/{len(polyT_list)}")
+        #Save the ROIs
+        print(f"{current_time()}: Saving rois for section {name_con_ele_pair} ({i+1}/{len(polyT_list)})")
         sys.stdout.flush()
-        np.savez(f'./processed_data/{spatial_index}/{name_con_ele_pair}_rois.npz', **rois)
+        np.savez(f'./processed_data/{run}/{name_con_ele_pair}_rois.npz', **rois)
 
-        print(f"{current_time()}: Generating quality report")
+        #Generate quility report of the segmentation using quality_report
+        print(f"{current_time()}: Generating quality report for section {name_con_ele_pair} ({i+1}/{len(polyT_list)})")
         sys.stdout.flush()
-        quality_report(image, rois, spatial_index, name_con_ele_pair)
+        quality_report(image, rois, run, name_con_ele_pair)
 
 
 
-spatial_index = "spatial1"
+run = "spatial1"
 
 folder_with_stainings = "./raw_data/spatial1"
 
-create_rois(folder_with_stainings,spatial_index)
+create_rois(folder_with_stainings,run)
