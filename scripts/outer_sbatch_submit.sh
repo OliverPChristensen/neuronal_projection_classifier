3ccb2f250f0f9b38bc5520f4676d9c205fa01604
wqc597 <wqc597@esrumhead01fl.unicph.domain>
2024-04-10 16:15:31 +0200

    Merge branch 'parallel segmentation' into master

--- scripts/outer_sbatch_submit.sh
+++ scripts/outer_sbatch_submit.sh
@@ -9,7 +9,9 @@
 ##SBATCH --mem=16G
 #SBATCH --time=20-00:00:00
 
+cd /projects/cbmr_shared/people/wqc597/neuronal_projection_classifier
+
 run_indices=("spatial1" "spatial2" "spatial3")
 run_index=${run_indices[$SLURM_ARRAY_TASK_ID-1]}
 
-sbatch inner_sbatch_submit.sh $SLURM_ARRAY_TASK_ID $run_index
+sbatch scripts/inner_sbatch_submit.sh $SLURM_ARRAY_TASK_ID $run_index
